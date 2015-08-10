0714bb9545729db8f0dc4f37d9fcb44eaaa0ca6a
小太 <nospam@xiaotai.org>
2015-08-10 22:10:55 +1000

    Week 3: Initial files

---
+++ week03/testList.c
@@ -0,0 +1,16 @@
+// testList.c - testing DLList data type
+// Written by John Shepherd, March 2013
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include "DLList.h"
+
+int main(int argc, char *argv[])
+{
+	DLList myList;
+	myList = getDLList(stdin);
+	showDLList(stdout,myList);
+	assert(validDLList(myList));
+	return 0;
+}
